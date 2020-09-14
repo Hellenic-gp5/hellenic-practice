b85949c2b2ba31e27ecf46b577efdb9050601e8a
yojansaini <yojansaini@gmail.com>
2020-09-14 15:35:54 +0530

    vs commit

---
+++ practice/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <div style="text-align:center">
+    <h1>Welcome to Hellenic<h1>
+    <h2> Team members<h2>
+    <ol>
+    <li>Yojan Saini</li>
+    </ol
+    </div>
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'practice';
+}
