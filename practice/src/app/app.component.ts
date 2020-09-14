6441eed0ae5617bc32bc292cf60047a884545bab
yojansaini <yojansaini@gmail.com>
2020-09-14 15:43:22 +0530

    commit by yojan

--- practice/src/app/app.component.ts
+++ practice/src/app/app.component.ts
@@ -3,13 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <div style="text-align:center">
+    <div style="text-align:center; text-color:red;">//edited by yojan
     <h1>Welcome to Hellenic<h1>
     <h2> Team members<h2>
     <ol>
     <li>Yojan Saini</li>
     <li> Ruhi Sinha</li>
-    </ol
+     </ol>
     </div>
   `,
   styles: []
