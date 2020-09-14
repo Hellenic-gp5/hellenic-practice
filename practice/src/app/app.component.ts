55f0708e3db7375c8c8fd4e682718d7d185fde82
Hellenic-gp5 <hellenic.gp5@gmail.com>
2020-09-14 15:50:20 +0530

    Commit by Anish

--- practice/src/app/app.component.ts
+++ practice/src/app/app.component.ts
@@ -9,7 +9,7 @@ import { Component } from '@angular/core';
     <ol>
     <li>Yojan Saini</li>
     <li> Ruhi Sinha</li>
-    <li>Anish Yadav</li>
+    <li>Anish Yadav</li>S
     </ol
      </ol>
     </div>
