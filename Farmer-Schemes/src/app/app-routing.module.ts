7b389390e266996fdc51cc6076d4263f93267c58
Hellenic-gp5 <hellenic.gp5@gmail.com>
2020-09-19 20:55:06 +0530

    commit 1

--- Farmer-Schemes/src/app/app-routing.module.ts
+++ Farmer-Schemes/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', redirectTo:'/add', pathMatch:'full'},
+  {path: 'add', component:HomeComponent},
+  {path: 'list', component:LoginComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
