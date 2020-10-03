b3cb2a819c4483c568262f8b5f61abc3116ff0a4
yojansaini <yojansaini@gmail.com>
2020-10-03 15:41:10 +0530

    Insurance Rest Controller

---
+++ agrimensor/src/main/java/com/lti/rest/InsuranceRestController.java
@@ -0,0 +1,39 @@
+package com.lti.rest;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lti.entity.Insurance;
+import com.lti.entity.InsuranceClaim;
+import com.lti.service.InsuranceService;
+import com.lti.service.InsuranceServiceImpl;
+/**
+ * @author YOJAN java version 1.8
+ *
+ */
+@CrossOrigin
+@RestController
+public class InsuranceRestController {
+
+	@Autowired
+	private InsuranceService service;
+	
+
+	@GetMapping(value="/fetchpolicy", produces="application/json")
+	public Insurance FetchPolicy(@RequestParam int id)
+	{ return service.search(id);}
+	
+	@PostMapping(value="/applyclaim/{polid}",consumes="application/json")
+	public String claim(@PathVariable int polid, @RequestBody InsuranceClaim cl) {
+		service.addClaim(polid, cl);
+		return "Claim applied successfully";
+	}
+	
+}
