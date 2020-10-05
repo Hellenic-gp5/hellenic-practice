611984b58c113cc59ed19bdd1ca85e58065ac93a
Sakshisingh19 <Sakshisingh.212586@gmail.com>
2020-10-05 15:09:04 +0530

    farmer registration

--- Farmer-Schemes/src/app/app-routing.module.ts
+++ Farmer-Schemes/src/app/app-routing.module.ts
@@ -14,86 +14,147 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { SellRComponent } from './SellR/sellR.component';
 import { ClaimComponent } from './claim/claim.component';
 import { ApplyComponent } from './apply/apply.component';
+import { FarmerRegisterComponent } from '../app/farmer-register/farmer-register.component';
 import { elementEventFullName } from '@angular/compiler/src/view_compiler/view_compiler';
+import { from } from 'rxjs';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-// <--------------Home Screen Routes-------------->
-{ path: 'home', component: HomeComponent, //Home Screen Component
-children:[{path:'login',redirectTo:'/login'},
-{path:'register',redirectTo:'/signup'}] },
+  // <--------------Home Screen Routes-------------->
+  {
+    path: 'home',
+    component: HomeComponent, //Home Screen Component
+    children: [
+      { path: 'login', redirectTo: '/login' },
+      { path: 'register', redirectTo: '/signup' },
+    ],
+  },
 
-{ path: 'login', component: LoginComponent, //Login Screen Component
-  children:[{path:'home',redirectTo:'/home'},
-  {path:'register',redirectTo:'/signup'}
-]},
+  {
+    path: 'login',
+    component: LoginComponent, //Login Screen Component
+    children: [
+      { path: 'home', redirectTo: '/home' },
+      { path: 'register', redirectTo: '/signup' },
+    ],
+  },
 
-{ path: 'signup', component: RegistrationComponent, //Registration Component
-children:[{path:'home',redirectTo:'/home'},{path:'login',redirectTo:'/login'}] },
+  {
+    path: 'signup',
+    component: RegistrationComponent, //Registration Component
+    children: [
+      { path: 'home', redirectTo: '/home' },
+      { path: 'login', redirectTo: '/login' },
+    ],
+  },
 
-// <--------Bidder route----------->
+  {
+    path: 'farmersignup',
+    component: FarmerRegisterComponent, //FarmerRegistration Component
+    children: [
+      { path: 'home', redirectTo: '/home' },
+      { path: 'login', redirectTo: '/login' },
+    ],
+  },
+
+  // <--------Bidder route----------->
   { path: 'bidder', component: BidderComponent },
- //<---------------Admin Module route-------------> 
-  {path: 'admin', component: AdminWelcomeComponent, 
-  children:[{path:'user',redirectTo:'/user',pathMatch:'full'},
-  {path:'sell',redirectTo:'/sell',pathMatch:'full'},
-  {path:'market',redirectTo:'/market',pathMatch:'full'},
-  {path:'insurance', redirectTo:'/insurance',pathMatch:'full'}]},
+  //<---------------Admin Module route------------->
+  {
+    path: 'admin',
+    component: AdminWelcomeComponent,
+    children: [
+      { path: 'user', redirectTo: '/user', pathMatch: 'full' },
+      { path: 'sell', redirectTo: '/sell', pathMatch: 'full' },
+      { path: 'market', redirectTo: '/market', pathMatch: 'full' },
+      { path: 'insurance', redirectTo: '/insurance', pathMatch: 'full' },
+    ],
+  },
 
+  {
+    path: 'user',
+    component: UserComponent,
+    children: [
+      { path: 'sell', redirectTo: '/sell', pathMatch: 'full' },
+      { path: 'market', redirectTo: '/market', pathMatch: 'full' },
+      { path: 'insurance', redirectTo: '/insurance', pathMatch: 'full' },
+      { path: 'admin', redirectTo: '/admin', pathMatch: 'full' },
+    ],
+  },
 
-  {path: 'user', component: UserComponent,
-  children:[{path:'sell',redirectTo:'/sell',pathMatch:'full'},
-  {path:'market',redirectTo:'/market',pathMatch:'full'},
-  {path:'insurance', redirectTo:'/insurance',pathMatch:'full'},
-  {path:'admin',redirectTo:'/admin',pathMatch:'full'}]},
+  {
+    path: 'sell',
+    component: SellComponent,
+    children: [
+      { path: 'user', redirectTo: '/user', pathMatch: 'full' },
+      { path: 'market', redirectTo: '/market', pathMatch: 'full' },
+      { path: 'insurance', redirectTo: '/insurance', pathMatch: 'full' },
+      { path: 'admin', redirectTo: '/admin', pathMatch: 'full' },
+    ],
+  },
 
-  {path: 'sell', component: SellComponent,
-  children:[{path:'user',redirectTo:'/user',pathMatch:'full'},
-  {path:'market',redirectTo:'/market',pathMatch:'full'},
-  {path:'insurance', redirectTo:'/insurance',pathMatch:'full'},
-  {path:'admin',redirectTo:'/admin',pathMatch:'full'}]},
+  {
+    path: 'market',
+    component: MarketComponent,
+    children: [
+      { path: 'user', redirectTo: '/user', pathMatch: 'full' },
+      { path: 'sell', redirectTo: '/sell', pathMatch: 'full' },
+      { path: 'insurance', redirectTo: '/insurance', pathMatch: 'full' },
+      { path: 'admin', redirectTo: '/admin', pathMatch: 'full' },
+    ],
+  },
 
+  {
+    path: 'insurance',
+    component: InsuranceComponent,
+    children: [
+      { path: 'user', redirectTo: '/user', pathMatch: 'full' },
+      { path: 'sell', redirectTo: '/sell', pathMatch: 'full' },
+      { path: 'market', redirectTo: '/market', pathMatch: 'full' },
+      { path: 'admin', redirectTo: '/admin', pathMatch: 'full' },
+    ],
+  },
 
-  {path: 'market', component: MarketComponent,
-children:[ {path:'user',redirectTo:'/user',pathMatch:'full'},
-{path:'sell',redirectTo:'/sell',pathMatch:'full'},
-{path:'insurance', redirectTo:'/insurance',pathMatch:'full'},
-{path:'admin',redirectTo:'/admin',pathMatch:'full'}]},
+  //  <-----------------These are farmer module routes------------>
+  {
+    path: 'farmer',
+    component: DashboardComponent,
+    children: [
+      { path: 'claim', redirectTo: '/claim', pathMatch: 'full' },
+      { path: 'apply', redirectTo: '/apply', pathMatch: 'full' },
+      { path: 'add', redirectTo: '/add', pathMatch: 'full' },
+    ],
+  },
 
-{path: 'insurance', component: InsuranceComponent,
-children:[{path:'user',redirectTo:'/user',pathMatch:'full'},
-{path:'sell',redirectTo:'/sell',pathMatch:'full'},
-{path:'market',redirectTo:'/market',pathMatch:'full'},
-{path:'admin',redirectTo:'/admin',pathMatch:'full'}]},
+  {
+    path: 'apply',
+    component: ApplyComponent,
+    children: [
+      { path: 'farmer', redirectTo: '/farmer', pathMatch: 'full' },
+      { path: 'claim', redirectTo: '/claim', pathMatch: 'full' },
+      { path: 'add', redirectTo: '/add', pathMatch: 'full' },
+    ],
+  },
 
+  {
+    path: 'add',
+    component: SellRComponent,
+    children: [
+      { path: 'farmer', redirectTo: '/farmer', pathMatch: 'full' },
+      { path: 'claim', redirectTo: '/claim', pathMatch: 'full' },
+      { path: 'apply', redirectTo: '/apply', pathMatch: 'full' },
+    ],
+  },
 
-  
-  
-//  <-----------------These are farmer module routes------------> 
-{path:'farmer',component:DashboardComponent, 
-children:[ {path:'claim',redirectTo:'/claim',pathMatch:'full'},
-{path:'apply',redirectTo:'/apply',pathMatch:'full'},
-{path:'add',redirectTo:'/add',pathMatch:'full'}]  },
-
-
-{path:'apply', component:ApplyComponent ,
-children:[{path:'farmer', redirectTo:'/farmer',pathMatch:'full'},
- {path:'claim',redirectTo:'/claim',pathMatch:'full'},
- {path:'add',redirectTo:'/add',pathMatch:'full'}]},
-
-
-{path:'add', component:SellRComponent, 
-children:[{path:'farmer', redirectTo:'/farmer',pathMatch:'full'},
-{path:'claim',redirectTo:'/claim',pathMatch:'full'},
-{path:'apply',redirectTo:'/apply',pathMatch:'full'}]},
-
-
-{path:'claim', component:ClaimComponent,
-children:[{path:'farmer', redirectTo:'/farmer',pathMatch:'full'},
-{path:'add',redirectTo:'/add',pathMatch:'full'},
-{path:'apply',redirectTo:'/apply',pathMatch:'full'}]}
-
-
+  {
+    path: 'claim',
+    component: ClaimComponent,
+    children: [
+      { path: 'farmer', redirectTo: '/farmer', pathMatch: 'full' },
+      { path: 'add', redirectTo: '/add', pathMatch: 'full' },
+      { path: 'apply', redirectTo: '/apply', pathMatch: 'full' },
+    ],
+  },
 ];
 
 @NgModule({
--- Farmer-Schemes/src/app/farmer.service.ts
+++ Farmer-Schemes/src/app/farmer.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
+import { from } from 'rxjs';
 import { RegisterModel } from '../app/register.module';
+import { FarmerRegisterModel } from '../app/FarmerRegister.module';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FarmerService {
   regList: RegisterModel[] = [];
+  FarmerregList: FarmerRegisterModel[] = [];
 
   constructor() {}
   saveReg(reg: RegisterModel) {
     this.regList.push(reg);
+  }
+  saveFarmerReg(Farmerreg: FarmerRegisterModel) {
+    this.FarmerregList.push(Farmerreg);
   }
 }
