23fa17d8865029950bba24522cd34f81494f0bed
yojansaini <yojansaini@gmail.com>
2020-10-06 15:26:01 +0530

    Approval List

--- Farmer-Schemes/src/app/user/user.component.ts
+++ Farmer-Schemes/src/app/user/user.component.ts
@@ -1,4 +1,7 @@
+import { HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { User } from '../User.module';
 
 @Component({
   selector: 'app-user',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-
-  constructor() { }
+ users:User[]=[];
+  constructor(private service: AuthService ) {}
 
   ngOnInit() {
+    this.service.getUserApprovalList().subscribe((data) => (this.users = data));
   }
-
+  reject(){
+  //   const params = new HttpParams()
+  //     .append("status", "Rejected");
+  //   this.service.
+   }
+   accept(){}
 }
