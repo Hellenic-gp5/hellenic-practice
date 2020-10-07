1a53069ce705a9a17f75b81f23fb6674f1e91b50
RRUHISINHA16 <rruhisinha16@gmail.com>
2020-10-07 19:41:51 +0530

    User-Approval

--- Farmer-Schemes/src/app/services/admin.service.ts
+++ Farmer-Schemes/src/app/services/admin.service.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@angular/core';
 import { FormGroup } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
-  baseUrl: string = 'http://localhost:8081/agrimensor/rest';
+  baseUrl: string = 'http://localhost:8080/agrimensor/rest';
 
   constructor(private http: HttpClient) { }
+updateUserStatus(userId:number, status:string)
+{
+  const params = new HttpParams()
+      .append("userStatus", status);
+  return this.http
+  .get(this.baseUrl + '/userstatus?userId=' + userId, {params:params}).toPromise();
+}
 }
