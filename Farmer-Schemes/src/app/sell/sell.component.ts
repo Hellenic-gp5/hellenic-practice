cd96fc6ce4b88bb1fc9357ba992cb1874387d4b8
RRUHISINHA16 <rruhisinha16@gmail.com>
2020-10-07 22:47:38 +0530

    admin-sell-request

--- Farmer-Schemes/src/app/sell/sell.component.ts
+++ Farmer-Schemes/src/app/sell/sell.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { SellRequest } from '../sellrequest.module';
+import { SellreqService } from '../services/sellreq.service';
 @Component({
   selector: 'app-sell',
   templateUrl: './sell.component.html',
   styleUrls: ['./sell.component.css']
 })
 export class SellComponent implements OnInit {
-
-  constructor() { }
+sellreq : SellRequest[]=[];
+cropId:number;
+  constructor(private service : SellreqService) { }
 
   ngOnInit() {
+    this.service.getSellRequest().subscribe((data) => (this.sellreq = data));
   }
+  rejected(cropId) {
+    this.service.updateCropStatus(cropId, 'Rejected')
+    }
+    unsold(cropId) {
+      this.service.updateCropStatus(cropId, 'Approved') 
+    }
 
 }
---
+++ Farmer-Schemes/src/app/services/sellreq.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { SellRequest } from '../sellrequest.module';
+@Injectable({
+  providedIn: 'root'
+})
+export class SellreqService {
+  baseUrl: string = 'http://localhost:8081/agrimensor/rest';
+  constructor(private http: HttpClient) {}
+  getSellRequest()
+  {
+    return this.http.get<SellRequest[]>(this.baseUrl+"/marketcrops");
+  }
+  updateCropStatus(cropId:number, status:string)
+{
+  const params = new HttpParams()
+      .append("cropSoldStatus", status);
+  return this.http
+  .get(this.baseUrl + '/updateCropStatus?cropId=' + cropId, {params:params}).toPromise();
+}
+}
