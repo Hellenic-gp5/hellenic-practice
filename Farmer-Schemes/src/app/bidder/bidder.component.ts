c7ce1963afeef1e01bd58e067f78adf477120df9
Sakshisingh19 <Sakshisingh.212586@gmail.com>
2020-10-08 12:50:48 +0530

    login page

--- Farmer-Schemes/src/app/bidder/bidder.component.ts
+++ Farmer-Schemes/src/app/bidder/bidder.component.ts
@@ -6,6 +6,7 @@ import { BidderService } from './../services/bidder.service';
 import { from } from 'rxjs';
 import { Bids } from './../bids.module';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-bidder',
@@ -14,34 +15,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BidderComponent implements OnInit {
   toggle: Boolean = false;
-  bname:string;
+  bname: string;
   // Bidding History Array
-   bids : Bids[] = [];
-   crops : items[] = [];
-   cropId : number;
-   bid : hell;
+  bids: Bids[] = [];
+  crops: items[] = [];
+  cropId: number;
+  bid: hell;
   stringifiedData: string;
   parsedJson: any;
 
-   constructor(private service : BidderService, private service2 : FarmerDashboardService) {}
+  constructor(
+    private service: BidderService,
+    private service2: FarmerDashboardService,
+    private router: Router
+  ) {}
 
- 
-  ngOnInit(): void { 
-    this.bname = localStorage.getItem("uname");
-    this.service.getBidHistory().subscribe((data) =>(this.bids = data));
+  ngOnInit(): void {
+    this.bname = localStorage.getItem('uname');
+    this.service.getBidHistory().subscribe((data) => (this.bids = data));
     this.service2.viewMarketPlace().subscribe((data) => (this.crops = data));
   }
 
- 
   applyBid(u: number) {
     this.cropId = u;
     this.toggle = !this.toggle;
   }
 
-  makeBid(){
+  makeBid() {
     this.stringifiedData = JSON.stringify(this.bid);
     this.parsedJson = JSON.parse(this.stringifiedData);
     this.service.setBid(this.parsedJson);
     this.service.addBid(this.cropId);
+    this.router.navigate(['/bidder']);
   }
 }
